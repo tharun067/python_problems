2fd890333b2de13b4d5d44a7ec88e01e917cbf6a
tharun067 <tharunchava126@gmail.com>
2025-01-08 12:56:23 +0530

    file deleted

--- day-4.py
+++
@@ -1,9 +0,0 @@
-
-def sum(n):
-    total=0
-    for i in range(n):
-        if i%2==0:
-            total+=i
-    return total
-n=int(input("Enter the input value: "))
-print("The sum of n even numbers: ",sum(n))
